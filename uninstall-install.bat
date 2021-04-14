8cb1c660c6cb8d97eab58862498ff75fe5767dbf
Zeram <zeram3@gmail.com>
2021-04-14 19:06:44 -0400

    Update uninstall-install.bat

--- uninstall-install.bat
+++ uninstall-install.bat
@@ -18,8 +18,10 @@ wmic datafile where name="C:\\Windows\\System32\\termsrv.dll" get Version /value
 @REM http://www.openmutual.org/2011/08/gnu-wget-missing-libeay32-dll-on-new-install-on-windows/
 @REM https://redfern.me/skipping-certificate-checks-with-wget/
 @REM https://serverfault.com/questions/171369/how-to-allow-wget-to-overwrite-files
-"%~dp0wget.exe" --no-check-certificate https://raw.githubusercontent.com/zeram/rdpwrap/master/res/rdpwrap.ini --output-document="%~dp0rdpwrap.ini"
-"wget.exe" --no-check-certificate https://raw.githubusercontent.com/zeram/rdpwrap/master/res/rdpwrap.ini --output-document="rdpwrap.ini"
+@REM "%~dp0wget.exe" --no-check-certificate https://raw.githubusercontent.com/zeram/rdpwrap/master/res/rdpwrap.ini --output-document="%~dp0rdpwrap.ini"
+"%~dp0wget.exe" --no-check-certificate "https://raw.githubusercontent.com/asmtron/rdpwrap/master/res/rdpwrap.ini --output-document="rdpwrap.ini"
+@REM "wget.exe" --no-check-certificate https://raw.githubusercontent.com/zeram/rdpwrap/master/res/rdpwrap.ini --output-document="rdpwrap.ini"
+"wget.exe" --no-check-certificate "https://raw.githubusercontent.com/asmtron/rdpwrap/master/res/rdpwrap.ini --output-document="rdpwrap.ini"
 
 
 @echo off
